85e163ad35375a810d4c1e86d6f63c6b19ac6b0f
Jon Weldon <jweldon@mines.edu>
2014-03-07 12:17:24 -0700

    Setup for inputEngine header

--- header/core/inputEngine.h
+++ header/core/inputEngine.h
@@ -1,29 +1,31 @@
-
-struct core::clickable{
-};
-
-typedef void (clickable::* mhandler)();
-typedef void (* handler)();
-
 namespace core{
-	struct keyHandler{
-		handler callback;
-		unsigned int key;
-	};
-	
-	struct mouseHandler{
-		mhandler callback_over;
-		mhandler callback_out;
-		mhandler callback_down_l;
-		mhandler callback_up_l;
-		mhandler callback_down_r;
-		mhandler callback_up_r;
-		float x;
-		float y;
-		float xsize;
-		float ysize;
-	};
-	
-	struct inputEngine{
-		static void postMsg();
-		static void registerHandler(void (*)())
+	namespace input{
+		struct clickable{
+			virtual void over;
+			virtual void out;
+			virtual void down_l;
+			virtual void up_l;
+			virtual void down_r;
+			virtual void up_r;
+			
+			float x;
+			float y;
+			float xsize;
+			float ysize;
+		};
+
+		struct keyObj{
+			virtual void call;
+			unsigned int key;
+		}
+		
+		struct inputEngine{
+			static void vector<clickable> mouseEvents;
+			static void vector<keyObj> keyEvents;
+			static MSG postMsg(MSG);
+			static void registerMouse(clickable*);
+			static void registerKey(clickable*);
+			static void bound(unsigned int);
+		}
+	}
+}
